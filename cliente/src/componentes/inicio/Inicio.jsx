df06eef486b7d84e9bd1df2c03b5afee1d24ee5f
jhostynac <jhostyn802@gmail.com>
2023-08-21 23:15:31 -0500

    pequeños cambios

--- cliente/src/componentes/inicio/Inicio.jsx
+++ cliente/src/componentes/inicio/Inicio.jsx
@@ -1,4 +1,4 @@
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 import "../inicio/inicio.css"
 
 export default function Principal() {
@@ -28,7 +28,7 @@ export default function Principal() {
                 <div class="texto2">
                     <a href="">
                         <div class="botones">
-                            <i class="fa-solid fa-cow"></i>
+                            <img src="./imagenes/icons8-cuts-of-beef-40.png" alt="" />
                             <div class="info">
                                 <h4>res</h4>
                             </div>
@@ -36,7 +36,7 @@ export default function Principal() {
                     </a>
                     <a href="">
                         <div class="botones">
-                            <i class="fa-solid fa-piggy-bank"></i>
+                            <img src="./imagenes/icons8-cuts-of-pork-40.png" alt="" />
                             <div class="info">
                                 <h4>cerdo</h4>
                             </div>
@@ -44,7 +44,7 @@ export default function Principal() {
                     </a>
                     <a href="">
                         <div class="botones">
-                            <i class="fa-solid fa-drumstick-bite"></i>
+                            <img src="./imagenes/icons8-pollo-40.png" alt="" />
                             <div class="info">
                                 <h4>pollo</h4>
                             </div>
@@ -52,7 +52,7 @@ export default function Principal() {
                     </a>
                     <a href="">
                         <div class="botones">
-                            <i class="fa-solid fa-drumstick-bite"></i>
+                            <img src="./imagenes/icons8-killed-fish-40.png" alt="" />
                             <div class="info">
                                 <h4>Pescado</h4>
                             </div>
@@ -60,71 +60,57 @@ export default function Principal() {
                     </a>
                 </div>
             </div>
+            <div class="cuadro-exotic3">
+                </div>
+        </body > <footer>
+            <div class="imagen">
+                <img src="./imagenes/logo bn_Mesa de trabajo 1.png" alt="" />
+            </div>
+            <div class="contenido">
+                <div class="textos">
+                    <h4>Contacto</h4>
+                    <p>3147175537</p>
+                </div>
+                <div class="textos">
+                    <h4>Horarios</h4>
+                    <p>Nuestros servicios van desde las 8am hasta las 8pm de lunes a sabado <br />
+                        Los domingos Nuestros servicios van desde las 8am hasta las 2pm
+                    </p>
+                </div>
+                <div class="icon">
+                    <svg heigth="80" width="80">
+                        <circle cx="40" cy="40" stroke="white" stroke-width="4" fill="none"></circle>
+                    </svg>
+                    <i class='bx bxl-facebook-circle'></i>
+
+                </div>
+
+                <div class="icon">
+                    <svg heigth="80" width="80">
+                        <circle cx="40" cy="40" stroke="white" stroke-width="4" fill="none"></circle>
+                    </svg>
+                    <i class="fa-brands fa-tiktok"></i>
+
+                </div>
+                <div class="icon">
+                    <svg heigth="80" width="80">
+                        <circle cx="40" cy="40" stroke="white" stroke-width="4" fill="none"></circle>
+                    </svg>
+                    <i class='bx bxl-instagram'></i>
+                </div>
+                <div class="textos">
+                    <h4>Direccion</h4>
+                    <p>Vía Vda. Mazo, Santa Elena, Medellín, Antioquia</p>
+                </div>
+
+            </div>
             <div class="mapa">
                 <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d463.5251584926393!2d-75.49555875382457!3d6.233914035712934!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e4427ba57cfd6ed%3A0x9cf641e74848383d!2sPUNTO%20DE%20VENTA%20LA%20GRANJA!5e0!3m2!1ses!2sco!4v1684432900628!5m2!1ses!2sco"
                     width="1350" height="600" allowfullscreen="" loading="lazy"
                     referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
-            <div class="cuadro-exotic3">
-                <div class="slider">
-                    <div class="slide">
-                        <img src="image1.jpg" alt="Image 1" />
-                        <div class="caption">Imagen 1</div>
-                    </div>
-                    <div class="slide">
-                        <img src="image2.jpg" alt="Image 2" />
-                        <div class="caption">Imagen 2</div>
-                    </div>
-                    <div class="slide">
-                        <img src="image3.jpg" alt="Image 3" />
-                        <div class="caption">Imagen 3</div>
-                    </div>
-                </div>
-            </div>
-        </body><footer>
-                <div class="imagen">
-                    <img src="./imagenes/logo bn_Mesa de trabajo 1.png" alt="" />
-                </div>
-                <div class="contenido">
-                    <div class="textos">
-                        <h4>Contacto</h4>
-                        <p>3147175537</p>
-                    </div>
-                    <div class="textos">
-                        <h4>Horarios</h4>
-                        <p>Nuestros servicios van desde las 8am hasta las 8pm de lunes a sabado <br />
-                            Los domingos Nuestros servicios van desde las 8am hasta las 2pm
-                        </p>
-                    </div>
-                    <div class="icon">
-                        <svg heigth="80" width="80">
-                            <circle cx="40" cy="40" stroke="white" stroke-width="4" fill="none"></circle>
-                        </svg>
-                        <i class='bx bxl-facebook-circle'></i>
-
-                    </div>
-
-                    <div class="icon">
-                        <svg heigth="80" width="80">
-                            <circle cx="40" cy="40" stroke="white" stroke-width="4" fill="none"></circle>
-                        </svg>
-                        <i class="fa-brands fa-tiktok"></i>
-
-                    </div>
-                    <div class="icon">
-                        <svg heigth="80" width="80">
-                            <circle cx="40" cy="40" stroke="white" stroke-width="4" fill="none"></circle>
-                        </svg>
-                        <i class='bx bxl-instagram'></i>
-                    </div>
-                    <div class="textos">
-                        <h4>Direccion</h4>
-                        <p>Vía Vda. Mazo, Santa Elena, Medellín, Antioquia</p>
-                    </div>
-
-                </div>
-            </footer></>
+        </footer></>
     )
 }
 
@@ -132,5 +118,3 @@ window.addEventListener('scroll', function () {
     var nav = this.document.querySelector('nav');
     nav.classList.toggle('bajar_1', window.scrollY > 0);
 });
-
-
