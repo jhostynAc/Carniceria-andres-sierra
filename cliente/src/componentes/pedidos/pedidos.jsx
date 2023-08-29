144468be73298427e8c1320f01820685df7c01d4
Jonnier <alzatejonny39@gmail.com>
2023-08-29 14:30:57 -0500

    change

--- cliente/src/componentes/pedidos/pedidos.jsx
+++ cliente/src/componentes/pedidos/pedidos.jsx
@@ -42,7 +42,22 @@ export default function Pedidos() {
 
 
 <section>
-    
+    <h1 class="titulo">Cadena de abastecimientos</h1>
+    <p class="info">Mantener la higiene en la carnicería y cada etapa de producción, comercialización y conservación
+         de la carne animal, es una necesidad para la salud de los consumidores y de los mismos operarios.
+         . De no cumplirse con las normas de higiene, existe el riesgo inminente de generarse problemas de
+          salud y enfermedades tipo ETA "enfermedades de transmisión por alimentos" como:
+Intoxicaciones.
+Envenenamiento de tipo agudo y crónico.
+Infecciones gastrointestinales por microorganismos como la Salmonella y E.Coli.
+Falla orgánica múltiple. </p>
+<p class="info">En Colombia la carne de res cuenta con varios perfiles que constituyen su cadena cárnica
+comercial, es decir, para que el producto llegue al consumidor final previamente ha pasado entre
+4 a 6 agentes intermediarios que incrementan el precio final, y es una debilidad que podemos
+identificar y principalmente está afectando directamente al productor en cuanto a sus ganancias
+totales obtenidas por el producto y a la cadena en cuanto a su desarrollo y productividad.Posteriormente debe incluirse un proceso de Abastecimiento, el cual permite establecer
+relaciones con un mercado proveedor de insumos y servicios, los cuales se requieren para el
+proceso siguiente que es el de producción en el cual se incorporan los insumos </p>
  <div class="galeria"> 
     <img src="https://tiendadecarnesfp.com/wp-content/uploads/2021/03/1-Tilapia-5-1-300x300.jpg" alt="" />
             <img src="https://tiendadecarnesfp.com/wp-content/uploads/2021/03/7-Espinazo-8-1.jpg" alt="" />
