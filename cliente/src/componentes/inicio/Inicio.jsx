a32cb7622d8ecd02a79197760674b90acff431dd
jhostynac <jhostyn802@gmail.com>
2023-08-29 22:09:17 -0500

    hola

--- cliente/src/componentes/inicio/Inicio.jsx
+++ cliente/src/componentes/inicio/Inicio.jsx
@@ -1,61 +1,78 @@
 import { Link } from "react-router-dom"
 import "../inicio/inicio.css"
-import { Carousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css'
 
 export default function Principal() {
     return (
+        <>
             <body>
-                <nav>
-                    <li >
-                        <Link href="./index.html" class="activa">Inicio</Link>
-                        <Link to={"/conocenos"}>Quienes somos</Link>
-                    </li>
-                    <img src="./imagenes/Sin título-1_Mesa de trabajo 1.png" alt="" />
-                    <li>
-                        <Link to={"/pedidos"}>Productos</Link>
-                        <Link to={"/ventas"}>Contacto</Link>
-                    </li>
-                </nav>
-                <div class="cuadro-exotic1">
-                    <img src="./imagenes/logo bn_Mesa de trabajo 1.png" alt="" />
+                <div className="caja">
+                    <nav>
+                        <li >
+                            <Link href="./index.html" class="activa">Inicio</Link>
+                            <Link to={"/conocenos"}>Quienes somos</Link>
+                            <Link to={"/pedidos"}>Productos</Link>
+                            <Link to={"/ventas"}>Contacto</Link>
+                        </li>
+                        <img src="./imagenes/Sin título-1_Mesa de trabajo 1.png" alt="" />
+                    </nav>
+                    <div className="inform">
+                        <h1>Calidad y corte</h1>
+                        <p>
+                            Disfruta de los cortes más deliciosos de res
+                            y cerdo en Medellín y el Oriente Antioqueño.
+                        </p>
+                    </div>
+
                 </div>
-                <div class="texto2">
-                    <a href=".">
-                        <div class="botones">
-                            <img src="./imagenes/icons8-cuts-of-beef-40.png" alt="" />
-                            <div class="info">
-                                <h4>res</h4>
-                            </div>
+                <div className="caja_inicio">
+                    <h4 className="lineas">Catalogo</h4>
+                    <div className="cont">
+                    <div class="card">
+                        <div class="card-content">
+                            <h2>Escapada natural</h2>
+                            <p>Recarga tu energía en un entorno tranquilo y verde.</p>
                         </div>
-                    </a>
-                    <a href=".">
-                        <div class="botones">
-                            <img src="./imagenes/icons8-cuts-of-pork-40.png" alt="" />
-                            <div class="info">
-                                <h4>cerdo</h4>
-                            </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-content">
+                            <h2>Escapada natural</h2>
+                            <p>Recarga tu energía en un entorno tranquilo y verde.</p>
                         </div>
-                    </a>
-                    <a href="">
-                        <div class="botones">
-                            <img src="./imagenes/icons8-pollo-40.png" alt="" />
-                            <div class="info">
-                                <h4>pollo</h4>
-                            </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-content">
+                            <h2>Escapada natural</h2>
+                            <p>Recarga tu energía en un entorno tranquilo y verde.</p>
                         </div>
-                    </a>
-                    <a href="">
-                        <div class="botones">
-                            <img src="./imagenes/icons8-killed-fish-40.png" alt="" />
-                            <div class="info">
-                                <h4>Pescado</h4>
-                            </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-content">
+                            <h2>Escapada natural</h2>
+                            <p>Recarga tu energía en un entorno tranquilo y verde.</p>
                         </div>
-                    </a>
+                    </div>
+                    </div>
                 </div>
-  
-            </body >
+                <div className="caja2_inicio">
+                <h4 className="lineas">servicio</h4>
+                <div className="conte">
+                    <div className="tarjeta">
+                    <h3>domicilio</h3>
+                    <img src="https://camponoble.co/wp-content/uploads/2020/05/icono-domicilios-2.png" alt="" />
+                    <p>oe la buena compra bien melo </p>
+                </div>
+                <div className="tarjeta">
+                    <h3>domicilio</h3>
+                    <img src="https://camponoble.co/wp-content/uploads/2020/05/icono-domicilios-2.png" alt="" />
+                    <p>oe la buena compra bien melo </p>
+                </div>
+                </div>
+                </div>
+                
+            </body > <footer>
+
+            </footer>
+        </>
     )
 }
 
