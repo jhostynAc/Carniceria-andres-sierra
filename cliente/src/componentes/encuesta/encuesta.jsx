e12110e8534547377c425dc17fa6801bbee11f59
jhostynac <jhostyn802@gmail.com>
2023-11-01 19:36:14 -0500

    cambios

--- cliente/src/componentes/encuesta/encuesta.jsx
+++ cliente/src/componentes/encuesta/encuesta.jsx
@@ -21,14 +21,14 @@ export default function MiFormulario() {
                             <Card.Img variant="top" src="./imagenes/joven_carnico.jpg" />
                             <Card.Body>
                                 <Card.Title style={{ color: 'white', fontSize: '20px', marginTop: '25px',  }}>Empleado</Card.Title>
-                                <Link className="Link" to={'https://docs.google.com/forms/d/e/1FAIpQLScQKYGLmx58u5L6gA_FcXfTS4f2G2hhu56SeZ3XjsSBeeJ2Vw/viewform'} target="_blanck"><Button variant="primary" style={{ backgroundColor: '#660708', border: 'none', marginTop: '25px', marginLeft: '15px' }}>Hacer</Button></Link>
+                                <Link className="Link" to={'https://docs.google.com/forms/d/e/1FAIpQLScQKYGLmx58u5L6gA_FcXfTS4f2G2hhu56SeZ3XjsSBeeJ2Vw/viewform'} target="_blanck"><Button variant="primary" style={{ backgroundColor: '#660708', border: 'none', marginTop: '25px', marginLeft: '15px' }}>Realizar</Button></Link>
                             </Card.Body>
                         </Card>
                         <Card style={{ width: '18rem', alignItems: 'center', backgroundColor: '#161a1d', border: '1px solid gray', }}>
                             <Card.Img variant="top" src="./imagenes/carnicería_4.jpg" />
                             <Card.Body>
                                 <Card.Title style={{ color: 'white', fontSize: '20px', marginTop: '25px' }}>Domicilio</Card.Title>
-                                <Link className="Link" to={'https://docs.google.com/forms/d/e/1FAIpQLSfsuu8OVsCnWrN2zVfv_YNbjcogTKbeDmAj_4yHw3vZdyyAvw/viewform'} target="_blanck"><Button variant="primary" style={{ backgroundColor: '#660708', border: 'none', marginTop: '25px', marginLeft: '15px' }}>Hacer</Button></Link>
+                                <Link className="Link" to={'https://docs.google.com/forms/d/e/1FAIpQLSfsuu8OVsCnWrN2zVfv_YNbjcogTKbeDmAj_4yHw3vZdyyAvw/viewform'} target="_blanck"><Button variant="primary" style={{ backgroundColor: '#660708', border: 'none', marginTop: '25px', marginLeft: '15px' }}>Realizar</Button></Link>
                             </Card.Body>
                         </Card>
                     </div>
